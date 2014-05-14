0a5500afa33b7c3435785b8ff190d2f5fbedf689
carlosjml4 <carlosjml4@hotmail.com>
2014-05-14 07:43:52 -0500

    traduccion comentarios

--- code/src/apps/ch.2.1/main.go
+++ code/src/apps/ch.2.1/main.go
@@ -1,11 +1,11 @@
-// Example code for Chapter ? from "Build Web Application with Golang"
-// Purpose: Hello world example demonstrating UTF-8 support.
-// To run in the console, type `go run main.go`
-// You're missing language fonts, if you're seeing squares or question marks.
+// Código de ejemplo para el capítulo? de "Build aplicación Web con Golang"
+// Propósito: ejemplo Hola mundo demostrando soporte UTF-8.
+// Para ejecutar en la consola, escriba `go run main.go
+// No debe ver cuadrados o signos de interrogación
 package main
 
 import "fmt"
 
 func main() {
-    fmt.Printf("Hello, world or 你好，世界 or καλημ ́ρα κóσμ or こんにちは世界\n")
+	fmt.Printf("Hello, world or 你好，世界 or καλημ ́ρα κóσμ or こんにちは世界\n")
 }
